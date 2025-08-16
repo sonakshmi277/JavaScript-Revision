d21745ed2d3b8f7f30ff3fa89f4bc94ddb01df14
Sonakshmi Bhattacharya <sonakshmibhattacharya@gmail.com>
2025-08-16 23:14:16 +0530

    Array

---
+++ forEach.js
@@ -0,0 +1,4 @@
+var arr=[1,2,3]
+var newArr=[]
+arr.forEach((num)=>newArr.push(num));
+console.log(newArr);
---
+++ map.js
@@ -0,0 +1,3 @@
+var arr=[1,2,3];
+var newArr=arr.map((num)=>(num*2));
+console.log(newArr);
